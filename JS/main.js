3a76202f10d264c7603e4c5cd327e68a7bba2eee
Mahdi-Falahati <falahati01mahdi@gmail.com>
2022-12-31 00:17:13 +0330

    completed live search

--- JS/main.js
+++ JS/main.js
@@ -7,42 +7,72 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     showData(promiseOfSomeData);
 });
+// collection of movies
+let Movies = [];
 
+// get tag with class cards
+const cards = document.querySelector(".cards");
 const showData = (response) => {
-    // get tag with class cards
-    const cards = document.querySelector(".cards");
-    // show respone in page 
-    response.forEach((movie) => {
+    // show respone in page
+    Movies = response.map((movie) => {
+        const { name, summary, url, airdate, id } = movie;
+        let { season, number } = movie;
         // show fetched data
         const card = `
-<section class="card">
-    <div class="card__content">
-    <section class="card__summary">
-            <span>Summary : </span>
-            ${movie.summary}
-            <a href="${movie.url}" class="card__link" target="_blank">
-                    Play Move..
-                </a>
-    </section>
-    <h1 class="card__name">${movie.name}</h1>
-        <img class="card__img" src="${movie.image.medium}" alt="${movie.name}" title="${movie.name}">
-        <section class="card__info">
-            <div class="card__inf-seri">
-                <span>S${movie.season <= 9 ? (movie.season = `0${movie.season}`) : (movie.season = movie.season)}</span>
-                /
-                <span>E${movie.number <= 9 ? (movie.number = `0${movie.number}`) : (movie.number = movie.number)}</span>
-            </div>
-            <div class="card__inf-imdb">
-                <span>IMDB ${movie.rating.average}</span>
-            </div>
-        </section>
-    </div>
-    <hr>
-    <div class="card__airdate"> Airdate ${movie.airdate}</div>
-</section>
-    `;
-        // added card as html to page 
-        cards.innerHTML+=card
+            <section class="card" id="Id${id}">
+                <div class="card__content">
+                    <section class="card__summary">
+                        <span>Summary : </span>
+                        ${summary}
+                        <a href="${url}" class="card__link" target="_blank">
+                            Play Move..
+                        </a>
+                    </section>
+                    <h1 class="card__name">${name}</h1>
+                    <img class="card__img" src="${movie.image.medium}" alt="${name}" title="${name}">
+                    <section class="card__info">
+                        <div class="card__inf-seri">
+                            <span>S${season <= 9 ? (season = `0${season}`) : (season = season)}</span>
+                            /
+                            <span>E${number <= 9 ? (number = `0${number}`) : (number = number)}</span>
+                        </div>
+                        <div class="card__inf-imdb">
+                            <span>IMDB ${movie.rating.average}</span>
+                        </div>
+                    </section>
+                </div>
+                <div class="card__airdate"> Airdate ${airdate}</div>
+         </section>
+        `;
+        // added card as html to page
+        cards.innerHTML += card;
+        return {
+            name,
+            summary,
+            id,
+        };
     });
-}
+};
 
+//search live
+const search = document.querySelector("#search");
+search.addEventListener("input", (e) => {
+    Movies.forEach((movie) => {
+        // check input value of summary and name
+        const isVisible = movie.name.toLowerCase().includes(e.target.value.toLowerCase()) || movie.summary.toLowerCase().includes(e.target.value.toLowerCase());
+        // add hide  & active class to cards for check cards without equal to summary or name
+        if (!isVisible) {
+            let id = "#Id" + movie.id;
+            const card = document.querySelector(id);
+            if (!card.classList.value.includes("hide")) {
+                card.classList += " hide";
+            } else {
+                card.classList.replace("active", "hide");
+            }
+        } else {
+            let id = "#Id" + movie.id;
+            const card = document.querySelector(id);
+            card.classList.replace("hide", "active");
+        }
+    });
+});
